8e7492c9b7a65c43eee8537465c6aa677f94b114
yimeiw <yimei.winata@binus.ac.id>
2026-08-11 19:58:54 +0800

    add branch fe edit

---
+++ CustomJewelryDesigner/CustomJewelryDesigner/View/Edit/MannequinView.swift
@@ -0,0 +1,18 @@
+//
+//  MannequinView.swift
+//  CustomJewelryDesigner
+//
+//  Created by Yimei Winata on 11/08/26.
+//
+
+import SwiftUI
+
+struct MannequinView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MannequinView()
+}
---
+++ CustomJewelryDesigner/CustomJewelryDesigner/View/Edit/RingView.swift
@@ -0,0 +1,18 @@
+//
+//  RingView.swift
+//  CustomJewelryDesigner
+//
+//  Created by Yimei Winata on 11/08/26.
+//
+
+import SwiftUI
+
+struct RingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RingView()
+}
